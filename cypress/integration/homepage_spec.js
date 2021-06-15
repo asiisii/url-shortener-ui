2488e2b988af42d63e0aef962c731600b5e150af
Ashish Malla <ashishmalla45@gmail.com>
2021-06-15 11:17:26 -0600

    Add test to iteration3.2 and iteration3.3

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -15,4 +15,14 @@ describe('Homepage', () => {
       .get('p')
       .contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
+
+  it.only('should be able to see Form and proper inputs', () => {
+    cy.get('form')
+      .find('input').should('have.length', 2)
+      .get('button').should('have.text', 'Shorten Please!')
+      .get('input[name="title"]').type('Test2')
+      .should('have.value', 'Test2')
+      .get('input[name="urlToShorten"]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      .should('have.value', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  })
 })
