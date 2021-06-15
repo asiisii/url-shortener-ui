564c5f4f85cc3b44cbb7fc94651a937e70161e9a
Ashish Malla <ashishmalla45@gmail.com>
2021-06-15 09:44:27 -0600

    Add a post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,15 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+
+export const postUrls = newUrl => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify(newUrl),
+    headers: {
+      "Content-type": "application/json"
+    }
+  })
+  
+}
