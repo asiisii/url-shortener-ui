174985a0d547dd3aa1d7ee241c5e6b7bcb913511
Ashish Malla <ashishmalla45@gmail.com>
2021-06-15 11:10:25 -0600

    Add test to iteration3.1

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -6,5 +6,13 @@ describe('Homepage', () => {
 
   it('should be able to see the page title', () => {
     cy.get('.App')
+      .get('header')
+      .get('h1').should('have.text','URL Shortener')
+      .get('.url')
+      .get('h3').should('have.text','Test 1')
+      .get('a')
+      .contains('http://localhost:3001/useshorturl/1')
+      .get('p')
+      .contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
 })
